dbcd5dfcd40fbfaa8e85a9ddddd25f0a30f31a81
trdxlucifer <singhdeepu566@gmail.com>
2022-05-27 19:03:15 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const inputs= document.querySelectorAll(".input");
+
+function focusFun(){
+    let parent = this.parentNode;
+    parent.classList.add("focus");
+}
+
+function blurFun(){
+    let parent =this.parentNode;
+    parent.classList.remove("focus");
+}
+
+inputs.forEach((input) =>{
+    input.addEventListener("focus", focusFun);
+    input.addEventListener("blur",blurFun);
+});
