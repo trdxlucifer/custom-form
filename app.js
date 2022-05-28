f48b24e7adf3b8d02bbecbdfe7b1095ea01bc095
trdxlucifer <singhdeepu566@gmail.com>
2022-05-28 20:09:34 +0530

    first commit

--- app.js
+++ app.js
@@ -18,6 +18,24 @@ inputs.forEach((input) => {
 });
 
 
-function SendEmail() {
-    console.log("Hello how are u ")
-}
+function Mail() {
+
+ var name = document.getElementById("name").value;
+ var email = document.getElementById("mail").value;
+ var phone =document.getElementById("phone").value;
+ var text = document.getElementById("text").value;
+ var body = 'name' +name +'<br/> email' +email+'<br/> phone'+phone +'<br/> text'+text
+   
+ Email.send({
+      Host: "smtp.gmail.com",
+      Username: "snghdamon@gmail.com",
+      Password: "lybbbgyemcznombk",
+      To: 'snghdamon@gmail.com',
+      From: document.getElementById("mail").value,
+      Subject: "New Enquiry of Contact Form",
+      Body: body
+    }).then(
+      message => alert("Message sent")
+    );
+  }
+
