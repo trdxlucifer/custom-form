b584226c2bc393da77dd9858884e5a4fd57eeeb8
trdxlucifer <singhdeepu566@gmail.com>
2022-05-28 20:14:14 +0530

    first commit

--- app.js
+++ app.js
@@ -19,13 +19,7 @@ inputs.forEach((input) => {
 
 
 function Mail() {
-
- var name = document.getElementById("name").value;
- var email = document.getElementById("mail").value;
- var phone =document.getElementById("phone").value;
- var text = document.getElementById("text").value;
- var body = 'name' +name +'<br/> email' +email+'<br/> phone'+phone +'<br/> text'+text
-   
+ 
  Email.send({
       Host: "smtp.gmail.com",
       Username: "snghdamon@gmail.com",
@@ -35,7 +29,8 @@ function Mail() {
       Subject: "New Enquiry of Contact Form",
       Body: body
     }).then(
-      message => alert("Message sent")
+      message => alert("Message sent Succefully")
     );
   }
+
 
